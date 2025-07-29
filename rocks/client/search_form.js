6bac90bf41bbec56b742a948669a63627b7354c0
SebastianBengt <william.widerberg@icloud.com>
2025-07-29 14:39:45 +0200

    worked on searching forrocks on the search_form, stopped it from changing page

--- rocks/client/search_form.js
+++ rocks/client/search_form.js
@@ -1,16 +1,19 @@
-const search_button = document.getElementById("extract_data")
-
-search_button.addEventListener('click', function(event){
-    let input_elt = document.getElementById('search_term')
-
-    alert("button clicked " + input_elt.value)
-
-})
 
 const search_form = document.getElementById('search_form')
-search_form.addEventListener('submit', function(){
+    search_form.addEventListener('submit', handleSearch)
+    search_form.addEventListener('keyup', handleSearch)
+    async function handleSearch(event){
+    event.preventDefault()
     let input_elt = document.getElementById('search_term')
     let search_term = input_elt.value
-    alert("button clicked " + input_elt.value)
+    let url = "http://127.0.0.1:8090/rock/search?search_term="+ search_term
+    let response = await fetch(url)
+    let body = await response.text();
+    let results = JSON.parse(body)
+    let results_list = document.getElementById('search_results')
+    results_list.innerHTML = ""
+    for(let result of results){
+        results_list.innerHTML += "<li>" + result.name + "</li>"
+    }
 
-})
+}
